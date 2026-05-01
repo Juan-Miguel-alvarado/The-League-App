66b843fa46cebfdaf41e00f60fa7a1c358f93675
JuanCho <juanchomigue2012@gmail.com>
2026-04-30 20:41:24 -0500

    Fix Netlify function path routing using rawUrl

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,18 +2,21 @@ const API_KEY = '0d9c408b126f46a685e9127195a29d7e'
 const BASE_URL = 'https://api.football-data.org/v4'
 
 exports.handler = async (event) => {
-  const apiPath = event.path.replace('/.netlify/functions/api', '') || '/'
-  const query = event.rawQuery ? `?${event.rawQuery}` : ''
-  const apiUrl = `${BASE_URL}${apiPath}${query}`
-
   try {
+    const url = new URL(event.rawUrl)
+    const apiPath = url.pathname.replace(/^\/api/, '') || '/'
+    const apiUrl = `${BASE_URL}${apiPath}${url.search}`
+
     const response = await fetch(apiUrl, {
       headers: { 'X-Auth-Token': API_KEY },
     })
     const body = await response.text()
     return {
       statusCode: response.status,
-      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
       body,
     }
   } catch (err) {
